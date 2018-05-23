e07f9ca8798a84b9f56b4a3efab608aeafc15994
Kyle <kyleverret@yahoo.com>
2018-05-23 17:59:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function theBeatlesPlay(musicians, instruments){
   var newArr = [];
-  for (var i=0; i<musicians.length-1; i++){
+  for (var i=0; i<musicians.length; i++){
     newArr.push(musicians[i] + " plays " + instruments[i]);
   }
   return newArr;
 }
 
 function johnLennonFacts(arr){
-  for (var i=0; i<arr.length-1; i++){
+  for (var i=0; i<arr.length; i++){
     arr[i] = arr[i]+"!!!";
   }
   return arr;
