a8144402287db2325161bc70a5ae169acb890264
Kyle <kyleverret@yahoo.com>
2018-05-23 17:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var newArr = [];
-  
+  for (var i=0; i<musicians.length-1; i++){
+    newArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 }
